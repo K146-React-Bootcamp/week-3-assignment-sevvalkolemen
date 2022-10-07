fd9939a772063d753e09faaa026a2ada0dfa18e9
sevvalkolemen <seval.kolemen@gmail.com>
2022-10-07 16:02:45 +0300

    sorting and pagination completed

--- day1/src/components/todo-list/index.js
+++ day1/src/components/todo-list/index.js
@@ -5,91 +5,142 @@ import classes from "./style.module.css";
 const url = "https://jsonplaceholder.typicode.com/todos";
 
 const TodoList = () => {
-	const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
   const [selectedTodo, setSelectedTodo] = useState();
+  const [itemSize, setItemSize] = useState(15);
+  const [currentPage, setCurrentPage] = useState(1);
 
-	useEffect(() => {
-		fetch(url)
-			.then((response) => response.json())
-			.then((todos) => {
-				setTodos(todos);
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	}, []);
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((todos) => {
+        setTodos(todos);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-	const renderThead = () => {
-		return (
-			<thead>
-				<tr>
-					<th>id</th>
-					<th>başlık</th>
-					<th>durum</th>
-					<th>Aksiyon</th>
-				</tr>
-			</thead>
-		);
-	};
+  // Id'ye göre sorting işlemleri
+  let idSorting = () => {
+    if (selectedTodo !== "id") {
+      setTodos([...todos].reverse());
+    } else {
+      setTodos([...todos].sort((a, b) => a.id - b.id));
+    }
+  };
 
+  // Durum bilgisine göre sorting işlemleri
+  let completedSorting = () => {
+	if (todos[0].completed) {
+		setTodos([...todos].sort((a, b) => a.completed - b.completed));
+	} else {
+		setTodos([...todos].sort((a, b) => b.completed - a.completed));
+	}
+  }
+
+  // Pagination işlemleri
+  const pages = [];
+  const start = currentPage * itemSize;
+  const end = start - itemSize;
+  const currentTodo = todos.slice(end,start);
+  
+  const renderPagination = () => {
+	let total = todos.length;
+	
+	const paginate = (pages) => setCurrentPage(pages);
+	for (let i = 1; i <= Math.ceil(total / itemSize); i++) {
+		pages.push(i)
+	}
+
+	return (
+		<nav>
+			<ul className="pagination">
+				{pages.map(num => (
+					<li key={num} className="page-item">
+						<a onClick={() => paginate(num)} href="#" className="page-link">{num}</a>
+					</li>
+				))}
+			</ul>
+		</nav>
+	)
+  }
+
+  const renderThead = () => {
+    return (
+      <thead>
+        <tr>
+          <th onClick={idSorting} className={classes.click}>id</th>
+          <th>başlık</th>
+          <th onClick={completedSorting} className={classes.click}>durum</th>
+          <th>Aksiyon</th>
+        </tr>
+      </thead>
+    );
+  };
 
   const remove = (todo) => {
     if (window.confirm("Silmek üzerisiniz emin misiniz")) {
-      setTodos(prev => {
-        return prev.filter(x => x.id != todo.id)
-      })
+      setTodos((prev) => {
+        return prev.filter((x) => x.id != todo.id);
+      });
     }
-  }
+  };
 
   const edit = (todo) => {
     setSelectedTodo(todo);
-  }
+  };
 
-	const renderBody = () => {
-		return (
-			<tbody>
-				{todos.slice(0, 15).map((todo, index) => {
-					return (
-						<tr key={index}>
-							<td>{todo.id}</td>
-							<td>{todo.title}</td>
-							<td>{todo.completed ? "Tamamlandı" : "Yapılacak"}</td>
-							<td>
-								<Button
+  const renderBody = () => {
+    return (
+      <tbody>
+        {currentTodo.slice(0, 15).map((todo, index) => {
+          return (
+            <tr key={index}>
+              <td>{todo.id}</td>
+              <td>{todo.title}</td>
+              <td>{todo.completed ? "Tamamlandı" : "Yapılacak"}</td>
+              <td>
+                <Button
                   className={`btn btn-sm btn-danger ${classes.actionButton} `}
                   onClick={() => remove(todo)}
-								>
-									Sil
-								</Button>
-								<Button onClick={() => edit(todo)} className="btn btn-sm btn-warning">Düzenle</Button>
-							</td>
-						</tr>
-					);
-				})}
-			</tbody>
-		);
-	};
-
+                >
+                  Sil
+                </Button>
+                <Button
+                  onClick={() => edit(todo)}
+                  className="btn btn-sm btn-warning"
+                >
+                  Düzenle
+                </Button>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    );
+  };
 
   const renderEditForm = () => {
     return (
       <div>
-        <input type={"text"}/>
-        <inpu type="check" />
+        <input type={"text"} />
+        <input type="check" />
         <Button>Kaydet</Button>
         <Button onClick={() => setSelectedTodo(undefined)}>Vazgeç</Button>
       </div>
-    )
-  }
-	return (
+    );
+  };
+  return (
     <div className={`${classes.container} container`}>
-      { selectedTodo && renderEditForm()}
-			<table className="table">
-				{renderThead()}
+      {selectedTodo && renderEditForm()}
+      <table className="table">
+        {renderThead()}
         {renderBody()}
-			</table>
-		</div>
-	);
+      </table>
+	  {renderPagination()}
+    </div>
+  );
 };
 
 export default TodoList;
